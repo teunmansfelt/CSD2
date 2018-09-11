13c5215d9db02d3651e4477d2a58122ab155b5ac
Teun Mansfelt <tmansfelt@gmail.com>
2018-09-11 16:26:53 +0200

    Simple audio playback. Added a few annotations

--- python_basics/playsound.py
+++ python_basics/playsound.py
@@ -2,12 +2,12 @@ import simpleaudio as sa
 
 #--------------------------- FUNCTIONS -------------------------#
 
-def loop(file_path, times) :
+def loop(file_path, times) : # Plays an audiofile a specified number of times
 	while(times > 0) :
 		playFile(file_path)
 		times -= 1
 
-def playFile(file_path) :
+def playFile(file_path) : # Plays an audiofile
 	wave_obj = sa.WaveObject.from_wave_file(file_path)
 	play_obj = wave_obj.play()
 	play_obj.wait_done()
@@ -20,12 +20,12 @@ print("(must be a whole number from 1 to 10)")
 
 valid = False
 
-while not valid :
+while not valid : # Keeps asking for an input if the given input is invalid
 	insert = input(">> ")
 
-	if(insert.isdigit()) :
-		insert = int(insert)
-		if(insert >= 1 and insert <= 10) :
+	if(insert.isdigit()) : # Checks if the input is an integer
+		insert = int(insert) # Converts a valid input-string to an int
+		if(insert >= 1 and insert <= 10) : # Checks if the input lays from 1 to 10
 			valid = True
 			loop("geluid.wav", insert)
 		else :
